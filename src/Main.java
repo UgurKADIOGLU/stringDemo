dd18da7c19b2eaa29c914b7d7f33ebc2120d5b33
UgurKADIOGLU <ugrkadioglu@gmail.com>
2022-09-20 21:33:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String mesaj="        Bugün hava çok güzel";
+    System.out.println(mesaj.replace(" ","-"));
+    System.out.println(mesaj.substring(2,4));
+
+    for(String kelime:mesaj.split(" "))
+    System.out.println(kelime);
+    System.out.println(mesaj.toLowerCase());
+    System.out.println(mesaj.toUpperCase());
+    System.out.println(mesaj.trim());
+    }
+}
